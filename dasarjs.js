3868f999e765ca409e1a27392dac3d0b77ef6ad3
MadeSatya5 <madesatya505@gmail.com>
2025-02-25 20:53:39 +0700

    first commit

---
+++ dasarjs.js
@@ -0,0 +1,279 @@
+const data = [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      publicationDate: "1954-07-29",
+      author: "J. R. R. Tolkien",
+      genres: [
+        "fantasy",
+        "high-fantasy",
+        "adventure",
+        "fiction",
+        "novels",
+        "literature",
+      ],
+      hasMovieAdaptation: true,
+      pages: 1216,
+      translations: {
+        spanish: "El señor de los anillos",
+        chinese: "魔戒",
+        french: "Le Seigneur des anneaux",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.52,
+          ratingsCount: 630994,
+          reviewsCount: 13417,
+        },
+        librarything: {
+          rating: 4.53,
+          ratingsCount: 47166,
+          reviewsCount: 452,
+        },
+      },
+    },
+    {
+      id: 2,
+      title: "The Cyberiad",
+      publicationDate: "1965-01-01",
+      author: "Stanislaw Lem",
+      genres: [
+        "science fiction",
+        "humor",
+        "speculative fiction",
+        "short stories",
+        "fantasy",
+      ],
+      hasMovieAdaptation: false,
+      pages: 295,
+      translations: {},
+      reviews: {
+        goodreads: {
+          rating: 4.16,
+          ratingsCount: 11663,
+          reviewsCount: 812,
+        },
+        librarything: {
+          rating: 4.13,
+          ratingsCount: 2434,
+          reviewsCount: 0,
+        },
+      },
+    },
+    {
+      id: 3,
+      title: "Dune",
+      publicationDate: "1965-01-01",
+      author: "Frank Herbert",
+      genres: ["science fiction", "novel", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 658,
+      translations: {
+        spanish: "",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.25,
+          ratingsCount: 1142893,
+          reviewsCount: 49701,
+        },
+      },
+    },
+    {
+      id: 4,
+      title: "Harry Potter and the Philosopher's Stone",
+      publicationDate: "1997-06-26",
+      author: "J. K. Rowling",
+      genres: ["fantasy", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 223,
+      translations: {
+        spanish: "Harry Potter y la piedra filosofal",
+        korean: "해리 포터와 마법사의 돌",
+        bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+        portuguese: "Harry Potter e a Pedra Filosofal",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.47,
+          ratingsCount: 8910059,
+          reviewsCount: 140625,
+        },
+        librarything: {
+          rating: 4.29,
+          ratingsCount: 120941,
+          reviewsCount: 1960,
+        },
+      },
+    },
+    {
+      id: 5,
+      title: "A Game of Thrones",
+      publicationDate: "1996-08-01",
+      author: "George R. R. Martin",
+      genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+      hasMovieAdaptation: true,
+      pages: 835,
+      translations: {
+        korean: "왕좌의 게임",
+        polish: "Gra o tron",
+        portuguese: "A Guerra dos Tronos",
+        spanish: "Juego de tronos",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.44,
+          ratingsCount: 2295233,
+          reviewsCount: 59058,
+        },
+        librarything: {
+          rating: 4.36,
+          ratingsCount: 38358,
+          reviewsCount: 1095,
+        },
+      },
+    },
+  ];
+  
+  function getBooks() {
+    return data;
+  }
+  
+  function getBook(id) {
+    return data.find((d) => d.id === id);
+  }
+  
+  
+  // --------------- Destructuring -----------------
+  const book = getBook(3);
+  book;
+  
+  // const title = book.title;
+  // const author = book.author;
+  
+  const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+    book; // Destructuring Object
+  console.log(author, title);
+  genres;
+  
+  // const primaryGenre = genres[0];
+  // const secondaryGenre = genres[1];
+  
+  const [primaryGenre, secondaryGenre, a, ...otherGenres] = genres; // Destructuring Array, ...(Rest / Spread) Harus diakhir
+  console.log(primaryGenre, secondaryGenre, a, otherGenres);
+  
+  
+  
+  // -------------- Rest / Spread Operator ---------------
+  const newGenres = [...genres, "epic fantasy"]; // Bisa di depan atau belakang Array
+  newGenres;
+  
+  const updatedBook = { // Object
+    ...book,
+    // Adding a New Property
+    moviePublicationDate: "2001-12-19",
+  
+    //Overriding an existing property
+    pages: 1120,
+  }; 
+  updatedBook;
+  
+  
+  
+  // --------------- Template Literals ----------------
+  const summary = `${title} is a book`;
+  summary;
+  
+  // --------------- Ternary Operators ----------------
+  const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+  pagesRange
+  const pagesRange1 = updatedBook.pages > 1000 ? 'over a thousand' : 'less than 1000';
+  pagesRange1
+  
+  // --------------- Arrow Functions ----------------------
+  // function getYear(str){
+  //   return str.split("-")[0];
+  // }                              //  disebut function declaration
+  
+  const getYear = (str) => str.split("-")[0];  //disebut function expression
+  
+  console.log(getYear(publicationDate));
+  
+  
+  // ------------------- Short Circuiting -------------------
+  console.log(true && "Some String");
+  console.log(false && "Some String");
+  console.log(hasMovieAdaptation && "This book has a movie")
+  
+  // falsy: 0, '', null, undefined
+  console.log("jonas" && "hello")
+  console.log(0 && "hello")
+  
+  console.log(true || "Some String");
+  console.log(false || "Some String");
+  
+  console.log(book.translations.spanish);
+  
+  const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";   // Bisa untuk ngasi nilai default
+  spanishTranslation;
+  
+  const count = book.reviews.librarything.reviewsCount ?? "No Data";
+  count;
+  
+  
+  // ---------------- Optional Chaining ---------------------
+  function getTotalReviewCount(book){
+    const goodreads = book.reviews.goodreads.reviewsCount;
+    const librarything = book.reviews.librarything?.reviewsCount; 
+    return goodreads + librarything;
+  }
+  console.log(getTotalReviewCount(book));
+  
+  
+  // ---------------- Array Map Method ----------------------  Membuat Array baru tanpa ngubah array asli
+  const x = [1, 2, 3, 4, 5];
+  console.log(x.map((element) => element * 2)); // [2, 4, 6, 8, 10]
+  
+  const books = getBooks();
+  
+  const titles = books.map((book) => book.title);
+  titles;
+  
+  function getTotalReviewCount(book) {
+    const goodreads = book.reviews.goodreads.reviewsCount;
+    const librarything = book.reviews.librarything?.reviewsCount;
+    return goodreads + librarything;
+  }
+  
+  const essentialData = books.map((book) => ({
+    title: book.title,
+    author: book.author,
+    reviewsCount: getTotalReviewCount(book),
+  }));
+  essentialData;
+  
+  // ---------------- Array Filter Method -------------------- Membuat Array Baru yang Isinya Memenuhi Kondisi Fungsinya
+  const longBooks = books
+    .filter((book) => book.pages > 500)
+    .filter((book) => book.hasMovieAdaptation);
+  longBooks;
+  
+  const adventureBooks = books
+    .filter((book) => book.genres.includes("adventure"))
+    .map((book) => book.title);
+  adventureBooks;
+  
+  // ---------------- Array Reduce Method --------------------
+  const pagesAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+  pagesAllBooks;
+  
+  // ---------------- Array Sort Method ----------------------
+  const arr = [3, 7, 1, 9, 6];
+  const sortedArr = arr.slice().sort((a, b) => a - b);
+  sortedArr;
+  arr;
+  
+  const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+  sortedByPages;
+
+  
