51ee18b313e862970e6fa32ee029655a815de0d4
MadeSatya5 <madesatya505@gmail.com>
2025-02-28 14:11:34 +0700

    nambah code sampe looping

---
+++ codePPT.js
@@ -0,0 +1,89 @@
+// Variable
+function varScoping() {
+  var x = 1;
+
+  if (true) {
+    var x = 2;
+    console.log(x); // print 2
+  }
+
+  console.log(x); // print 2
+}
+
+function letScoping() {
+  let x = 1;
+
+  if (true) {
+    let x = 2;
+    console.log(x); // print 2
+  }
+
+  console.log(x); // print 1
+}
+
+const pi = 3.14;
+console.log(pi); // print 3.14
+
+
+// Tipe Data Primitif
+let teks = "hello world"; //String
+let x = 10; // Number
+let bigInt = 12345678901234567890n // Bigint
+let isOpen = true; // Boolean
+let none; // Undefined
+let cat = null; // null
+let uniqueId = Symbol("Halo"); // Symbol
+
+//Tipe Data Non Primitif
+let user = {
+  name: "Budi",
+  age: 20,
+}
+let numbers = [1, 2, 3, 4, 5];
+function sayHello() {
+  return "Hello World";
+}
+
+
+// Percabangan
+// if else
+let nilai = 80;
+
+if (nilai >= 90) {
+  console.log("A");
+} else if (nilai >= 75) {
+  console.log("B");
+} else {
+  console.log("C");
+}
+
+// Ternary Operator
+const number = 20;
+const result = number > 10 ? "Number is bigger than 10" : "Number is smaller than 10"
+console.log(result); // Print "Number is bigger than 10"
+
+// Switch
+let fruit = "apple";
+
+switch (fruit) {
+  case "apple":
+    console.log("It's an apple!"); // Print
+    break;
+  case "banana":
+    console.log("It's a banana!");
+    break;
+  default:
+    console.log("Unknown fruit.");
+}
+
+
+//Perulangan
+for (let i = 1; i <= 5; i++) {
+  console.log("Iterasi " + i);
+}
+
+let i = 1;
+while (i <= 3) {
+  console.log("Number " + i);
+  i++;
+}
--- dasarjs.js
+++ dasarjs.js
@@ -79,60 +79,6 @@ const data = [
         },
       },
     },
-    {
-      id: 4,
-      title: "Harry Potter and the Philosopher's Stone",
-      publicationDate: "1997-06-26",
-      author: "J. K. Rowling",
-      genres: ["fantasy", "adventure"],
-      hasMovieAdaptation: true,
-      pages: 223,
-      translations: {
-        spanish: "Harry Potter y la piedra filosofal",
-        korean: "해리 포터와 마법사의 돌",
-        bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
-        portuguese: "Harry Potter e a Pedra Filosofal",
-      },
-      reviews: {
-        goodreads: {
-          rating: 4.47,
-          ratingsCount: 8910059,
-          reviewsCount: 140625,
-        },
-        librarything: {
-          rating: 4.29,
-          ratingsCount: 120941,
-          reviewsCount: 1960,
-        },
-      },
-    },
-    {
-      id: 5,
-      title: "A Game of Thrones",
-      publicationDate: "1996-08-01",
-      author: "George R. R. Martin",
-      genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
-      hasMovieAdaptation: true,
-      pages: 835,
-      translations: {
-        korean: "왕좌의 게임",
-        polish: "Gra o tron",
-        portuguese: "A Guerra dos Tronos",
-        spanish: "Juego de tronos",
-      },
-      reviews: {
-        goodreads: {
-          rating: 4.44,
-          ratingsCount: 2295233,
-          reviewsCount: 59058,
-        },
-        librarything: {
-          rating: 4.36,
-          ratingsCount: 38358,
-          reviewsCount: 1095,
-        },
-      },
-    },
   ];
   
   function getBooks() {
